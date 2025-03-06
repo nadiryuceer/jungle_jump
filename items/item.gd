f771bc2888aa2e0866d03c6b0f0851cdd7691663
Nadir Yüceer <nadir.yuceer@gmail.com>
2025-03-06 21:33:05 +0300

    First level - IN PROGRESS

---
+++ items/item.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+signal picked_up
+
+var textures = {
+	"cherry": "res://assets/sprites/cherry.png",
+	"gem": "res://assets/sprites/gem.png"
+}
+func init(type, _position):
+	$Sprite2D.texture = load(textures[type])
+	position = _position
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_item_body_entered(body):
+	picked_up.emit()
+	queue_free()
--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,21 @@
 extends CharacterBody2D
 
 @export var gravity = 750
-@export var run_speed = 150
+@export var run_speed = 120
 @export var jump_speed = -300
 enum {IDLE, RUN, JUMP, HURT, DEAD}
 var state = IDLE
 
+signal life_changed
+signal died
+var life = 3: set = set_life
+
+
+func set_life(value):
+	life = value
+	life_changed.emit(life)
+	if life <= 0:
+		change_state(DEAD)
 
 func _ready():
 	change_state(IDLE)
@@ -20,13 +30,21 @@ func change_state(new_state):
 			$AnimationPlayer.play("run")
 		HURT:
 			$AnimationPlayer.play("hurt")
+			velocity.y = -200
+			velocity.x = -100 * sign(velocity.x)
+			life -= 1
+			await get_tree().create_timer(0.5).timeout
+			change_state(IDLE)
 		JUMP:
 			$AnimationPlayer.play("jump_up")
 		DEAD:
+			died.emit()
 			hide()
 
 
 func get_input():
+	if state == HURT:
+		return
 	var right = Input.is_action_pressed("right")
 	var left = Input.is_action_pressed("left")
 	var jump = Input.is_action_just_pressed("jump")
@@ -59,6 +77,13 @@ func _physics_process(delta):
 
 	move_and_slide()
 	
+	if state == HURT:
+		return
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		if collision.get_collider().is_in_group("danger"):
+			hurt()
+	
 	if state == JUMP and is_on_floor():
 		change_state(IDLE)
 	if state == JUMP and velocity.y > 0:
@@ -69,3 +94,8 @@ func reset(_position):
 	position = _position
 	show()
 	change_state(IDLE)
+	life = 3
+	
+func hurt():
+	if state != HURT:
+		change_state(HURT)
